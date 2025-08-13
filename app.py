b738e63d6d9e618b53cdd529b14724e6099ade2f
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2025-08-13 14:53:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import firebase_admin
+from firebase_admin import credentials, firestore, storage
 
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'storageBucket': "your-bucket-name.appspot.com",
+    'databaseURL': "https://your-project.firebaseio.com"
+})
+db = firestore.client()
+bucket = storage.bucket()
